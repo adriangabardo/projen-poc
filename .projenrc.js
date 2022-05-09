b35fe7b3a7cf77d5e7ddfe018ad95245eaaec329
Adrian Gabardo <adrian.gabardo@argenti.com.au>
2022-05-09 16:11:28 +1000

    nuget?

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,5 @@
 const { awscdk } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Adrian Gabardo',
   authorAddress: 'adrian.gabardo@argenti.com.au',
@@ -9,11 +10,20 @@ const project = new awscdk.AwsCdkConstructLibrary({
   mutableBuild: true,
   npmDistTag: 'latest',
   npmRegistryUrl: 'https://npm.pkg.github.com',
+  npmAccess: NpmAccess.RESTRICTED,
+  publishToNuget: {
+    dotNetNamespace: 'adriangabardo.projen.poc',
+    packageId: 'projen.poc',
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+});
+
+project.release.publisher.publishToNuget({
+  nugetApiKeySecret: 'NPM_TOKEN',
 });
 
 project.synth();
