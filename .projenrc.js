8c9178ed1d569458d0178512a016ae6642f9dba6
Adrian Gabardo <adrian.gabardo@argenti.com.au>
2022-05-09 11:37:27 +1000

    publish to GitHub Packages

--- .projenrc.js
+++ .projenrc.js
@@ -6,6 +6,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   defaultReleaseBranch: 'main',
   name: 'projen-poc',
   repositoryUrl: 'git@github.com:adriangabardo/projen-poc.git',
+  mutableBuild: true,
+  npmDistTag: 'latest',
+  npmRegistryUrl: 'https://npm.pkg.github.com',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
